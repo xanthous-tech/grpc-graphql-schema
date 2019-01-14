62bc5ace8027f316861edb6f27fc6b42bea73365
Simon Liang <no.lhr0909@gmail.com>
2019-01-14 18:13:30 +0700

    finished example grpc server

--- example/grpc/build/server.js
+++ example/grpc/build/server.js
@@ -4,12 +4,49 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const debug = require("debug");
 const grpc = require("grpc");
 const Example_grpc_pb_1 = require("./proto/Example_grpc_pb");
+const Example_pb_1 = require("./proto/Example_pb");
 const log = debug("SampleServer");
+const Movies = [
+    {
+        cast: ["Tom Cruise", "Simon Pegg", "Jeremy Renner"],
+        name: "Mission: Impossible Rogue Nation",
+        rating: 0.97,
+        year: 2015,
+    },
+    {
+        cast: ["Tom Cruise", "Simon Pegg", "Henry Cavill"],
+        name: "Mission: Impossible - Fallout",
+        rating: 0.93,
+        year: 2018,
+    },
+    {
+        cast: ["Leonardo DiCaprio", "Jonah Hill", "Margot Robbie"],
+        name: "The Wolf of Wall Street",
+        rating: 0.78,
+        year: 2013,
+    },
+];
+function createMovie(movie) {
+    const result = new Example_pb_1.Movie();
+    result.setCastList(movie.cast);
+    result.setName(movie.name);
+    result.setYear(movie.year);
+    result.setRating(movie.rating);
+    return result;
+}
 class ServerImpl {
     getMovies(request, callback) {
-        callback(new Error(""), null);
+        const result = new Example_pb_1.MoviesResult();
+        Movies.map(createMovie).forEach((movie) => result.addResult(movie));
+        callback(null, result);
     }
     searchMoviesByCast(call) {
+        const input = call.request;
+        Movies.map(createMovie).forEach((movie) => {
+            if (movie.getCastList().indexOf(input.getCastname()) > -1) {
+                call.write(movie);
+            }
+        });
         call.end();
     }
 }
--- example/grpc/src/server.ts
+++ example/grpc/src/server.ts
@@ -8,12 +8,59 @@ import { EmptyRequest, Movie, MoviesResult, SearchByCastInput } from "./proto/Ex
 
 const log = debug("SampleServer");
 
+interface IRawMovie {
+  cast: string[];
+  name: string;
+  rating: number;
+  year: number;
+}
+
+const Movies = [
+  {
+    cast: ["Tom Cruise", "Simon Pegg", "Jeremy Renner"],
+    name: "Mission: Impossible Rogue Nation",
+    rating: 0.97,
+    year: 2015,
+  },
+  {
+    cast: ["Tom Cruise", "Simon Pegg", "Henry Cavill"],
+    name: "Mission: Impossible - Fallout",
+    rating: 0.93,
+    year: 2018,
+  },
+  {
+    cast: ["Leonardo DiCaprio", "Jonah Hill", "Margot Robbie"],
+    name: "The Wolf of Wall Street",
+    rating: 0.78,
+    year: 2013,
+  },
+];
+
+function createMovie(movie: IRawMovie): Movie {
+  const result = new Movie();
+
+  result.setCastList(movie.cast);
+  result.setName(movie.name);
+  result.setYear(movie.year);
+  result.setRating(movie.rating);
+
+  return result;
+}
+
 class ServerImpl implements IExampleServer {
   public getMovies(request: grpc.ServerUnaryCall<EmptyRequest>, callback: grpc.sendUnaryData<MoviesResult>) {
-    callback(new Error(""), null);
+    const result = new MoviesResult();
+    Movies.map(createMovie).forEach((movie: Movie) => result.addResult(movie));
+    callback(null, result);
   }
 
   public searchMoviesByCast(call: grpc.ServerWriteableStream<SearchByCastInput>) {
+    const input: SearchByCastInput = call.request;
+    Movies.map(createMovie).forEach((movie: Movie) => {
+      if (movie.getCastList().indexOf(input.getCastname()) > -1) {
+        call.write(movie);
+      }
+    });
     call.end();
   }
 }
